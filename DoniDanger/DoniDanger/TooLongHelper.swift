d6228aa8d29f8657739219b1c0fd71c684abf7a7
Philipp Anger <philipp.anger@runtastic.com>
2016-11-24 10:13:31 +0100

    add too long file

---
+++ DoniDanger/DoniDanger/TooLongHelper.swift
@@ -0,0 +1,69 @@
+//
+//  TooLongHelper.swift
+//  DoniDanger
+//
+//  Created by Philipp Anger on 24/11/16.
+//  Copyright © 2016 Runtastic GmbH. All rights reserved.
+//
+
+import Foundation
+
+func one() -> Int {
+    return 1
+}
+
+func two() -> Int {
+    return 2
+}
+
+func three() -> Int {
+    return 3
+}
+
+func four() -> Int {
+    return 4
+}
+
+func five() -> Int {
+    return 5
+}
+
+func six() -> Int {
+    return 6
+}
+
+func seven() -> Int {
+    return 7
+}
+
+func eight() -> Int {
+    return 8
+}
+
+func nine() -> Int {
+    return 9
+}
+
+func ten() -> Int {
+    return 10
+}
+
+func eleven() -> Int {
+    return 11
+}
+
+func twelve() -> Int {
+    return 12
+}
+
+func thirteen() -> Int {
+    return 13
+}
+
+func fourteen() -> Int {
+    return 14
+}
+
+func fithteen() -> Int {
+    return 15
+}
--- DoniDanger/DoniDangerTests/DoniDangerTests.swift
+++ DoniDanger/DoniDangerTests/DoniDangerTests.swift
@@ -19,8 +19,26 @@ class DoniDangerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAddOne() {
         XCTAssertEqual(addToOne(toAdd: 5), 6)
+    }
+    
+    func testTooLongHelper() {
+        XCTAssertEqual(one(), 1)
+        XCTAssertEqual(two(), 2)
+        XCTAssertEqual(three(), 3)
+        XCTAssertEqual(four(), 4)
+        XCTAssertEqual(five(), 5)
+        XCTAssertEqual(six(), 6)
+        XCTAssertEqual(seven(), 7)
+        XCTAssertEqual(eight(), 8)
+        XCTAssertEqual(nine(), 9)
+        XCTAssertEqual(ten(), 10)
+        XCTAssertEqual(eleven(), 11)
+        XCTAssertEqual(twelve(), 12)
+        XCTAssertEqual(thirteen(), 13)
+        XCTAssertEqual(fourteen(), 14)
+        XCTAssertEqual(fithteen(), 15)
     }
     
 }
