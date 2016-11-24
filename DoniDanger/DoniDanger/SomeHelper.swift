26e4e966ff4466a3cdc35325f0bbe7059d5891e9
Philipp Anger <philipp.anger@runtastic.com>
2016-11-24 09:23:38 +0100

    add project

---
+++ DoniDanger/DoniDanger/SomeHelper.swift
@@ -0,0 +1,13 @@
+//
+//  SomeHelper.swift
+//  DoniDanger
+//
+//  Created by Philipp Anger on 24/11/16.
+//  Copyright © 2016 Runtastic GmbH. All rights reserved.
+//
+
+import Foundation
+
+func addToOne(toAdd: Int) -> Int {
+    return 1 + toAdd
+}
---
+++ DoniDanger/DoniDangerTests/DoniDangerTests.swift
@@ -0,0 +1,26 @@
+//
+//  DoniDangerTests.swift
+//  DoniDangerTests
+//
+//  Created by Philipp Anger on 24/11/16.
+//  Copyright © 2016 Runtastic GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import DoniDanger
+
+class DoniDangerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertEqual(addToOne(toAdd: 5), 6)
+    }
+    
+}
